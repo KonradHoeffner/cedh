c45332c604dc2d9affb09402c3074cb193d4f14c
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-01-23 12:50:52 +0100

    Finish first prototype.

--- dist/staples.js
+++ dist/staples.js
@@ -1,6 +1,8 @@
 function countCards() {
 	const cards = {};
 	const notfound = new Set();
+	identityCount = {};
+
 	for (const deckName in decks) {
 		for (const cardName of decks[deckName]) {
 			if (!cards[cardName]) {
@@ -14,34 +16,41 @@ function countCards() {
 			cards[cardName].count++;
 		}
 	}
+	// percentages
+	for (const cardName in cards) {
+		const card = cards[cardName];
+		cards[cardName].percent = Math.round((100 * card.count) / Object.keys(decks).length);
+	}
 	if (notfound.size > 0) console.warn("Not found in index: ", notfound);
 	console.log(cards);
 	return cards;
 }
 
-function createTable()
-{
+function createTable() {
 	const table = document.getElementById("staplesTable");
 
-	const keys = ["count","cmc","colors","color_identity"];
+	const keys = ["count", "percent", "cmc", "colors", "color_identity"];
 
-	const columnDefs = [{ field: "name", sortable: true, filter: 'agTextColumnFilter' },{ field: "count", sortable: true, filter: 'agNumberColumnFilter'},{ field: "cmc", sortable: true, filter: 'agNumberColumnFilter'},
-	{field: "colors", sortable: true, filter: 'agSetColumnFilter'}, {field: "color_identity", sortable: true, filter: 'agSetColumnFilter'}];
-	
+	const columnDefs = [
+		{ field: "name", sortable: true, filter: "agTextColumnFilter" },
+		{ field: "count", sortable: true, filter: "agNumberColumnFilter" },
+		{ field: "percent", sortable: true, filter: "agNumberColumnFilter" },
+		{ field: "cmc", sortable: true, filter: "agNumberColumnFilter" },
+		{ field: "colors", sortable: true, filter: "agSetColumnFilter" },
+		{ field: "color_identity", sortable: true, filter: "agSetColumnFilter" },
+	];
+
 	const cards = countCards();
-	const rowData = []
-	for(const cardName in cards)
-	{
+	const rowData = [];
+	for (const cardName in cards) {
 		const card = cards[cardName];
-		const entry = {name: cardName};
+		const entry = { name: cardName };
 		rowData.push(entry);
-		for(const key of keys)
-		{
-			entry[key]= card[key];
+		for (const key of keys) {
+			entry[key] = card[key];
 		}
 	}
-	const gridOptions = {columnDefs: columnDefs, rowData: rowData};
+	const gridOptions = { columnDefs: columnDefs, rowData: rowData };
 	const gridDiv = document.getElementById("staplesGrid");
-	new agGrid.Grid(gridDiv, gridOptions);	
+	new agGrid.Grid(gridDiv, gridOptions);
 }
-
