66d9610a1e96217aeee4d9d504b5406cbc8e025f
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-01-30 22:37:57 +0100

    New release with new attributes.

--- staples.js
+++ staples.js
@@ -1,48 +1,23 @@
-function countCards() {
-	const cards = {};
-	const notfound = new Set();
-	identityCount = {};
-
-	for (const deckName in decks) {
-		for (const cardName of decks[deckName]) {
-			if (!cards[cardName]) {
-				if (!mtgindex[cardName]) {
-					notfound.add(cardName);
-					continue;
-				}
-				cards[cardName] = mtgindex[cardName];
-				cards[cardName].count = 0;
-			}
-			cards[cardName].count++;
-		}
-	}
-	// percentages
-	for (const cardName in cards) {
-		const card = cards[cardName];
-		cards[cardName].percent = Math.round((100 * card.count) / Object.keys(decks).length);
-	}
-	if (notfound.size > 0) console.warn("Not found in index: ", notfound);
-	//console.log(cards);
-	return cards;
-}
-
 let gridOptions;
 
 function createTable() {
 	const table = document.getElementById("staplesTable");
 
-	const keys = ["count", "percent", "cmc", "colors", "color_identity"];
+	const keys = ["count", "percent", "supercent", "rank", "superrank","identity_rank", "cmc", "colors", "color_identity"];
 
 	const columnDefs = [
 		{ field: "name", sortable: true, filter: "agTextColumnFilter" },
 		{ field: "count", sortable: true, filter: "agNumberColumnFilter" },
 		{ field: "percent", sortable: true, filter: "agNumberColumnFilter" },
+		{ field: "rank", sortable: true, filter: "agNumberColumnFilter" },
+		{ field: "supercent", sortable: true, filter: "agNumberColumnFilter" },
+		{ field: "superrank", sortable: true, filter: "agNumberColumnFilter" },
+		{ field: "identity_rank", sortable: true, filter: "agNumberColumnFilter" },
 		{ field: "cmc", sortable: true, filter: "agNumberColumnFilter" },
 		{ field: "colors", sortable: true, filter: "agSetColumnFilter" },
 		{ field: "color_identity", sortable: true, filter: "agSetColumnFilter" },
 	];
 
-	const cards = countCards();
 	const rowData = [];
 	for (const cardName in cards) {
 		const card = cards[cardName];
