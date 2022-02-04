fe56db0c404449d909c6489332f5d7fe69d23b44
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-02-04 16:56:53 +0100

    Add markdown function.

--- py/cardstats.py
+++ py/cardstats.py
@@ -106,3 +106,15 @@ json_string = json.dumps(cardStats, indent=2)
 with open(STATFILE_JS, "w") as f_js:
     f_js.write("const cards =\n" + json_string + ";")
     print("Saved", STATFILE_JS)
+
+def markdown():
+    writer = MarkdownTableWriter(
+        headers=["int", "float", "str", "bool", "mix", "time"],
+        value_matrix=[
+            [0,   0.1,      "hoge", True,   0,      "2017-01-01 03:04:05+0900"],
+            [2,   "-2.23",  "foo",  False,  None,   "2017-12-23 45:01:23+0900"],
+            [3,   0,        "bar",  "true",  "inf", "2017-03-03 33:44:55+0900"],
+            [-10, -9.9,     "",     "FALSE", "nan", "2017-01-01 00:00:00+0900"],
+        ],
+    )
+    writer.write_table()
