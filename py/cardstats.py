2f5e5182e93f671007ad42e0015aa7184e53e041
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-02-05 14:01:39 +0100

    Add tables by color identity.

--- py/cardstats.py
+++ py/cardstats.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import sys
 from urllib.request import urlopen
 from itertools import combinations, chain
+
 # load stuff ################################################################
 
 INDEX_URL = "https://konradhoeffner.github.io/mtgindex/mtgindex.json"
@@ -43,64 +44,66 @@ for cardName in cardCounter:
     entry = index[cardName]
     entry["name"] = cardName
     entry["count"] = cardCounter[cardName]
-    entry["percent"] = int(100*cardCounter[cardName]/len(decks))
+    entry["percent"] = int(100 * cardCounter[cardName] / len(decks))
     entry["types"] = entry["type_line"].split("\u2014")[0].strip().split(" ")
     cardStats[cardName] = entry
-    
-    identityCounter[frozenset(entry["color_identity"])]+=1
+
+    identityCounter[frozenset(entry["color_identity"])] += 1
 
 # superidentity #############################################################
 for deckName in decks:
     deck = decks[deckName]
     commanders = deck["commanders"]
     # workaround for index problems
-    if commanders[0]=="Kenrith, the Returned King":
-        deck["color_identity"] = frozenset(["W","U","R","B","R","G"])
+    if commanders[0] == "Kenrith, the Returned King":
+        deck["color_identity"] = frozenset(["W", "U", "R", "B", "R", "G"])
         continue
-    deckIdentity = set().union(*list(map(lambda commander: frozenset(index[commander.split("//")[0].strip()]["color_identity"]),commanders)))
+    deckIdentity = set().union(
+        *list(map(lambda commander: frozenset(index[commander.split("//")[0].strip()]["color_identity"]), commanders))
+    )
     deck["color_identity"] = deckIdentity
 
 superIdentityCounter = Counter()
 for identity in identityCounter:
-    #superidentities = set(filter(lambda id: id.issuperset(identity), identityCounter.keys()))
-    #superdecks = filter(lambda deck: frozenset(deck["color_identity"]) in superidentities,decks.values())
-    superdecks = filter(lambda deck: frozenset(deck["color_identity"]).issuperset(identity),decks.values())
-    superIdentityCounter[identity]=len(list(superdecks))
+    # superidentities = set(filter(lambda id: id.issuperset(identity), identityCounter.keys()))
+    # superdecks = filter(lambda deck: frozenset(deck["color_identity"]) in superidentities,decks.values())
+    superdecks = filter(lambda deck: frozenset(deck["color_identity"]).issuperset(identity), decks.values())
+    superIdentityCounter[identity] = len(list(superdecks))
 
 # supercent as abbreviation for "super identity percentage" so table columns fit small screens
 for cardName in cardStats:
-    #print(cardName,frozenset(cardStats[cardName]["color_identity"]))
-    cardStats[cardName]["supercent"] = int(100*cardCounter[cardName]/superIdentityCounter[frozenset(cardStats[cardName]["color_identity"])])
+    # print(cardName,frozenset(cardStats[cardName]["color_identity"]))
+    cardStats[cardName]["supercent"] = int(
+        100 * cardCounter[cardName] / superIdentityCounter[frozenset(cardStats[cardName]["color_identity"])]
+    )
 
 # ranking ###################################################################
 
-def rank(cardNames,on,key):
+
+def rank(cardNames, on, key):
     cardNames.sort(key=lambda cardName: cardStats[cardName][on], reverse=True)
     for i in range(len(cardNames)):
-        cardStats[cardNames[i]][key] = i+1
+        cardStats[cardNames[i]][key] = i + 1
+
 
 # all cards
-rank(list(cardStats.keys()),"count","rank")
-rank(list(cardStats.keys()),"supercent","superrank")
+rank(list(cardStats.keys()), "count", "rank")
+rank(list(cardStats.keys()), "supercent", "superrank")
 # separately for each color identity
 for identity in identityCounter:
-   identityCards = list(filter(lambda cardName: frozenset(cardStats[cardName]["color_identity"])==identity, cardStats.keys()))
-   rank(identityCards,"count","identity_rank")
+    identityCards = list(filter(lambda cardName: frozenset(cardStats[cardName]["color_identity"]) == identity, cardStats.keys()))
+    rank(identityCards, "count", "identity_rank")
 
 #############################################################################
 
-#def powerset(iterable):
-#    s = list(iterable)
-#    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
-
 # color identity
-#COLORS = frozenset({"W","U","B","R","G"})
-#COLOR_IDENTITIES = frozenset(powerset(COLORS))
-#print(COLOR_IDENTITIES)
-#print(len(identityCounter),"color identities")
-#print(identityCounter)
+# COLORS = frozenset({"W","U","B","R","G"})
+# COLOR_IDENTITIES = frozenset(powerset(COLORS))
+# print(COLOR_IDENTITIES)
+# print(len(identityCounter),"color identities")
+# print(identityCounter)
 
-cardStats = dict(sorted(cardStats.items())) # sort to minimize diff
+cardStats = dict(sorted(cardStats.items()))  # sort to minimize diff
 
 with open(STATFILE, "w") as f:
     json.dump(cardStats, f, indent=2)
@@ -110,15 +113,3 @@ json_string = json.dumps(cardStats, indent=2)
 with open(STATFILE_JS, "w") as f_js:
     f_js.write("const cards =\n" + json_string + ";")
     print("Saved", STATFILE_JS)
-
-def markdown():
-    writer = MarkdownTableWriter(
-        headers=["int", "float", "str", "bool", "mix", "time"],
-        value_matrix=[
-            [0,   0.1,      "hoge", True,   0,      "2017-01-01 03:04:05+0900"],
-            [2,   "-2.23",  "foo",  False,  None,   "2017-12-23 45:01:23+0900"],
-            [3,   0,        "bar",  "true",  "inf", "2017-03-03 33:44:55+0900"],
-            [-10, -9.9,     "",     "FALSE", "nan", "2017-01-01 00:00:00+0900"],
-        ],
-    )
-    writer.write_table()
--- py/diff.py
+++ py/diff.py
@@ -1,6 +1,7 @@
 # compare dist/cards.json to dist/cards.json.old and give a markdown table report
 # requires checking out an old version of dist/cards.json to dist/cards.old.json
 import json
+from itertools import combinations, chain
 import pytablewriter as ptw
 
 CARDFILE = "./dist/cards.json"
@@ -9,7 +10,7 @@ DIFFFILE = "./dist/cards.diff.json"
 MIN_COUNT = 3
 MIN_DIFF = 5
 MIN_SUPER_DIFF = 1
-MAX_DIFF = 100  # workaround to exclude outliers caused by errors in the data
+MAX_DIFF = 1000  # workaround to exclude outliers caused by errors in the data
 
 with open(CARDFILE, "r") as f:
     cards = json.load(f)
@@ -34,20 +35,29 @@ for key in keys:
     superrank = new["superrank"]
     rankdiff = old["rank"] - rank
     superrankdiff = old["superrank"] - superrank
-    if abs(rank) < MIN_DIFF and abs(superrank) < MIN_SUPER_DIFF:
-        continue
-    if max(abs(rankdiff), abs(superrankdiff)) > MAX_DIFF:
-        continue
-    entry = {
-        "count": count,
-        "countdiff": countdiff,
-        "rank": rank,
-        "rankdiff": rankdiff,
-        "superrank": superrank,
-        "superrankdiff": superrankdiff,
-    }
-    matrix_entry = [key, count, countdiff, rank, rankdiff, superrank, superrankdiff]
-    diff[key] = entry
+    irank = new["identity_rank"]
+    irankdiff = old["identity_rank"] - irank
+    # if abs(rank) < MIN_DIFF and abs(superrank) < MIN_SUPER_DIFF:
+    #    continue
+    # if max(abs(rankdiff), abs(superrankdiff)) > MAX_DIFF:
+    #    continue
+    new["countdiff"] = countdiff
+    new["rankdiff"] = rankdiff
+    new["superrankdiff"] = superrankdiff
+    matrix_entry = [
+        key,
+        count,
+        countdiff,
+        rank,
+        rankdiff,
+        superrank,
+        superrankdiff,
+        new["color_identity"],
+        new["supercent"],
+        irank,
+        irankdiff,
+    ]
+    diff[key] = new
     diff_matrix.append(matrix_entry)
 
 with open(DIFFFILE, "w") as f:
@@ -55,11 +65,41 @@ with open(DIFFFILE, "w") as f:
     print("saved", len(keys), "diffs to", DIFFFILE)
 
 
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+
+
+COLORS = {"W": "White", "U": "Blue", "B": "Black", "R": "Red", "G": "Green"}
+COLOR_IDENTITIES = powerset(COLORS.keys())
+
+
 def markdown():
     headers = ["Card", "Count", "ΔCount", "Rank", "ΔRank", "SRank", "ΔSRank"]
-    
+
     byrankdiff = sorted(diff_matrix, key=lambda row: row[2], reverse=True)[0:5]
-    writer = ptw.MarkdownTableWriter(table_name="Top Upcounted", headers=headers, value_matrix=byrankdiff)
+    writer = ptw.MarkdownTableWriter(table_name="Top Increased Count", headers=headers, value_matrix=byrankdiff)
     writer.write_table()
+    print()
+    byrankdiff = sorted(diff_matrix, key=lambda row: row[2])[0:5]
+    writer = ptw.MarkdownTableWriter(table_name="Top Decreased Count", headers=headers, value_matrix=byrankdiff)
+    writer.write_table()
+
+    headers = ["Card", "Count", "ΔCount", "% rel"]
+    for identity in COLOR_IDENTITIES:
+        identity = set(identity)
+        byidentity = map(lambda row: row[0:3] + row[8:11], filter(lambda card: identity == set(card[7]), diff_matrix))
+        byidentity = sorted(byidentity, key=lambda row: row[1], reverse=True)[0:10]
+        if len(byidentity) < 1:
+            continue
+        identityName = "".join(identity)
+        if identityName in COLORS:
+            identityName = COLORS[identityName]
+        if identityName == "":
+            identityName = "Colorless"
+        print()
+        writer = ptw.MarkdownTableWriter(table_name="Top " + identityName, headers=headers, value_matrix=byidentity)
+        writer.write_table()
+
 
 markdown()
