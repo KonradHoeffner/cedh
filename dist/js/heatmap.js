fcc8f7ffcdfc9bc5a1ddd6652f3ec1b2a6dc022f
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-02-11 19:08:51 +0100

    Improve heatmaps.

--- dist/js/heatmap.js
+++ dist/js/heatmap.js
@@ -1,4 +1,4 @@
-function drawHeatMap(id) {
+function drawHeatMap(id, f) {
 	// set the dimensions and margins of the graph
 	const margin = { top: 80, right: 25, bottom: 30, left: 40 },
 		width = 1450 - margin.left - margin.right,
@@ -16,11 +16,27 @@ function drawHeatMap(id) {
 	//Read the data
 	d3.json("data/cards.json").then(function (data) {
 		data = Object.values(data);
-		creatures = data.filter((card) => "power" in card);
-
+		creatures = data.filter((card) => "power" in card && !isNaN(card.power) && "toughness" in card && !isNaN(card.toughness));
+		aggregate = [];
+		for (let p = 0; p < 13; p++)
+			for (let t = 0; t < 13; t++) {
+				c = data.filter((card) => card.power == p && card.toughness == t);
+				if (c.length === 0) continue;
+				intensity = c.map(f).reduce((a,b)=>a+b);
+				aggregate.push({
+					power: p,
+					toughness: t,
+					intensity: intensity,
+					names: c.map((card) => card.name).reduce((a, b) => a + ", " + b),
+				});
+			}
+		console.log(aggregate);
 		// Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
-		const myGroups = Array.from(new Set(creatures.map((d) => d.power)));
-		const myVars = Array.from(new Set(creatures.map((d) => d.toughness)));
+		const myGroups = Array.from(new Set(aggregate.map((d) => Number(d.power))));
+		const numerically = (a, b) => a - b;
+		myGroups.sort(numerically);
+		const myVars = Array.from(new Set(aggregate.map((d) => Number(d.toughness))));
+		myVars.sort(numerically);
 
 		// Build X scales and axis:
 		const x = d3.scaleBand().range([0, width]).domain(myGroups).padding(0.05);
@@ -29,9 +45,10 @@ function drawHeatMap(id) {
 		// Build Y scales and axis:
 		const y = d3.scaleBand().range([height, 0]).domain(myVars).padding(0.05);
 		svg.append("g").style("font-size", 15).call(d3.axisLeft(y).tickSize(0)).select(".domain").remove();
-
+		
 		// Build color scale
-		const myColor = d3.scaleSequential().interpolator(d3.interpolateInferno).domain([1, 100]);
+		const max = Math.max(...aggregate.map(entry=>entry.intensity));
+		const myColor = d3.scaleSequentialLog().interpolator(d3.interpolateYlOrRd).domain([1, max]);
 
 		// create a tooltip
 		const tooltip = d3
@@ -52,7 +69,7 @@ function drawHeatMap(id) {
 		};
 		const mousemove = function (event, d) {
 			tooltip
-				.html("The exact value of<br>this cell is: " + d.value)
+				.html(d.names+" "+Math.round(d.intensity*100)/100)
 				.style("left", event.x / 2 + "px")
 				.style("top", event.y / 2 + "px");
 		};
@@ -64,7 +81,7 @@ function drawHeatMap(id) {
 		// add the squares
 		svg
 			.selectAll()
-			.data(creatures, function (d) {
+			.data(aggregate, function (d) {
 				return d.power + ":" + d.toughness;
 			})
 			.join("rect")
@@ -79,7 +96,7 @@ function drawHeatMap(id) {
 			.attr("width", x.bandwidth())
 			.attr("height", y.bandwidth())
 			.style("fill", function (d) {
-				return myColor(d.count);
+				return myColor(d.intensity);
 			})
 			.style("stroke-width", 4)
 			.style("stroke", "none")
@@ -90,7 +107,7 @@ function drawHeatMap(id) {
 	});
 
 	// Add title to graph
-	svg.append("text").attr("x", 0).attr("y", -50).attr("text-anchor", "left").style("font-size", "22px").text("A d3.js heatmap");
+	svg.append("text").attr("x", 0).attr("y", -50).attr("text-anchor", "left").style("font-size", "22px");//.text("Power and toughness combinations");
 
 	// Add subtitle to graph
 	svg
@@ -100,6 +117,6 @@ function drawHeatMap(id) {
 		.attr("text-anchor", "left")
 		.style("font-size", "14px")
 		.style("fill", "grey")
-		.style("max-width", 400)
-		.text("A short description of the take-away message of this chart.");
+		.style("max-width", 400);
+		//.text("Power and thoughness values of creatures in the average deck.");
 }
