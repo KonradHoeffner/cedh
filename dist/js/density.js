487881845e8b5383803f7ae933ac2b14f93a2aa2
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-02-13 11:55:00 +0100

    Add color density.

--- dist/js/density.js
+++ dist/js/density.js
@@ -2,19 +2,19 @@ const margin = { top: 30, right: 30, bottom: 30, left: 50 };
 const width = 2460 - margin.left - margin.right;
 const height = 1400 - margin.top - margin.bottom;
 
-function count(id) {
+function count(id, attr, filter, color) {
 	// https://www.d3-graph-gallery.com/graph/density_slider.html
 	const svg = d3Svg(id, margin, width, height);
 
 	d3.json("data/cards.json").then(function (data) {
 		data = Object.values(data);
-		data.sort((a, b) => a.rank - b.rank);
-		data.push({ count: 0, rank: 0 }); // to close the curve
-		console.log(data);
+		data = data.filter(filter);
+		data.sort((a, b) => a[attr] - b[attr]);
+		const minX = Math.min(...data.map((card) => card[attr]));
 		// add the x Axis
-		const x = d3.scaleLinear().domain([0, data.length]).range([0, width]);
+		const x = d3.scaleLinear().domain([minX, data.length]).range([0, width]);
 		svg.append("g").attr("transform", `translate(0, ${height})`).call(d3.axisBottom(x));
-
+		data.push({ count: 0, [attr]: minX }); // to close the curve
 		// add the y Axis
 		const y = d3
 			.scaleLinear()
@@ -28,7 +28,7 @@ function count(id) {
 			.append("path")
 			//.attr("class", "mypath")
 			.datum(data)
-			.attr("fill", "#69b3a2")
+			.attr("fill", color)
 			.attr("opacity", ".8")
 			.attr("stroke", "#000")
 			.attr("stroke-width", 1)
@@ -39,7 +39,7 @@ function count(id) {
 					.line()
 					.curve(d3.curveBasis)
 					.x(function (d) {
-						return x(d.rank);
+						return x(d[attr]);
 					})
 					.y(function (d) {
 						return y(d.count);
