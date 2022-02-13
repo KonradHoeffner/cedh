8f6b727539fca1a1e6bb958d304c451b7b9e4c58
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2022-02-13 13:32:08 +0100

    Add log scale.

--- dist/js/density.js
+++ dist/js/density.js
@@ -2,24 +2,27 @@ const margin = { top: 30, right: 30, bottom: 30, left: 50 };
 const width = 2460 - margin.left - margin.right;
 const height = 1400 - margin.top - margin.bottom;
 
-function count(id, attr, filter, color) {
+function count(id, attr, filter, color, scale) {
+	// todo: remove attr
 	// https://www.d3-graph-gallery.com/graph/density_slider.html
 	const svg = d3Svg(id, margin, width, height);
 
 	d3.json("data/cards.json").then(function (data) {
 		data = Object.values(data);
 		data = data.filter(filter);
-		data.sort((a, b) => a[attr] - b[attr]);
-		const minX = Math.min(...data.map((card) => card[attr]));
+		data = data.filter((card) => card.count > 0); // workaround for buggy cards with count=0 which breaks log scale visualization
+		data.sort((a, b) => b.count - a.count);
+		//const minX = Math.min(...data.map((card) => card[attr]));
+		const minX = 1;
 		// add the x Axis
 		const x = d3.scaleLinear().domain([minX, data.length]).range([0, width]);
 		svg.append("g").attr("transform", `translate(0, ${height})`).call(d3.axisBottom(x));
-		data.push({ count: 0, [attr]: minX }); // to close the curve
-		// add the y Axis
-		const y = d3
-			.scaleLinear()
-			.range([height, 0])
-			.domain([0, Math.max(...data.map((card) => card.count))]);
+		for (let i = 0; i < data.length; i++) {
+			data[i].sortRank = i + 1;
+		}
+		data.push({ count: 1, sortRank: 1 }); // close the curve
+
+		const y = scale.range([height, 0]).domain([1, Math.max(...data.map((card) => card.count))]);
 		svg.append("g").call(d3.axisLeft(y));
 
 		// Plot the area
@@ -37,9 +40,9 @@ function count(id, attr, filter, color) {
 				"d",
 				d3
 					.line()
-					.curve(d3.curveBasis)
+					.curve(d3.curveCatmullRom)
 					.x(function (d) {
-						return x(d[attr]);
+						return x(d.sortRank);
 					})
 					.y(function (d) {
 						return y(d.count);
